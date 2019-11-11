ac79e7f186874185a5febcc15081ba760b1cfdb2
Pradeep <55576635+pradeep71d@users.noreply.github.com>
2019-11-11 16:11:22 +0530

    add

---
+++ app/src/main/java/com/example/demo12/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.demo12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String str = "pradeep";
+        int count = 0;
+        char[] ch = str.toCharArray();
+        System.out.println("Duplicate Characters are:");
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (ch[i] == ch[j]) {
+                    System.out.println(ch[j]);
+                    count++;
+                    break;
+                }
+            }
+        }
+    }
+}
